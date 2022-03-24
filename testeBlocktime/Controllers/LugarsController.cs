1d56e7c40a772d2202a948b6ce0dc0770250685a
Caio Henrique Do Nascimento Soares <53976212857@info.scs>
2022-03-24 11:29:07 -0300

    triste

--- testeBlocktime/Controllers/LugarsController.cs
+++ testeBlocktime/Controllers/LugarsController.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using testeBlocktime.Contexts;
 using testeBlocktime.Domains;
 
 namespace testeBlocktime.Controllers
 {
-    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
-    public class LugarsController : Controller
+    public class LugarsController : ControllerBase
     {
         private readonly testeContext _context;
 
@@ -22,131 +21,83 @@ namespace testeBlocktime.Controllers
             _context = context;
         }
 
-        // GET: Lugars
+        // GET: api/Lugars
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<ActionResult<IEnumerable<Lugar>>> GetLugars()
         {
-            return View(await _context.Lugars.ToListAsync());
+            return await _context.Lugars.ToListAsync();
         }
 
-        // GET: Lugars/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: api/Lugars/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Lugar>> GetLugar(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var lugar = await _context.Lugars
-                .FirstOrDefaultAsync(m => m.IdLugar == id);
-            if (lugar == null)
-            {
-                return NotFound();
-            }
-
-            return View(lugar);
-        }
-
-        // GET: Lugars/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Lugars/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdLugar,Latitude,Longitude")] Lugar lugar)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(lugar);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(lugar);
-        }
-
-        // GET: Lugars/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var lugar = await _context.Lugars.FindAsync(id);
+
             if (lugar == null)
             {
                 return NotFound();
             }
-            return View(lugar);
+
+            return lugar;
         }
 
-        // POST: Lugars/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdLugar,Latitude,Longitude")] Lugar lugar)
+        // PUT: api/Lugars/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLugar(int id, Lugar lugar)
         {
             if (id != lugar.IdLugar)
             {
-                return NotFound();
+                return BadRequest();
             }
 
-            if (ModelState.IsValid)
+            _context.Entry(lugar).State = EntityState.Modified;
+
+            try
             {
-                try
-                {
-                    _context.Update(lugar);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!LugarExists(lugar.IdLugar))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
             }
-            return View(lugar);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LugarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // GET: Lugars/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // POST: api/Lugars
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Lugar>> PostLugar(Lugar lugar)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            _context.Lugars.Add(lugar);
+            await _context.SaveChangesAsync();
 
-            var lugar = await _context.Lugars
-                .FirstOrDefaultAsync(m => m.IdLugar == id);
+            return CreatedAtAction("GetLugar", new { id = lugar.IdLugar }, lugar);
+        }
+
+        // DELETE: api/Lugars/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLugar(int id)
+        {
+            var lugar = await _context.Lugars.FindAsync(id);
             if (lugar == null)
             {
                 return NotFound();
             }
 
-            return View(lugar);
-        }
-
-        // POST: Lugars/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var lugar = await _context.Lugars.FindAsync(id);
             _context.Lugars.Remove(lugar);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return NoContent();
         }
 
         private bool LugarExists(int id)
--- testeBlocktime/Startup.cs
+++ testeBlocktime/Startup.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace testeBlocktime
@@ -22,7 +25,16 @@ namespace testeBlocktime
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TesteBlock.webAPI", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +53,19 @@ namespace testeBlocktime
 
             app.UseRouting();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TesteBlock.webAPI");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
